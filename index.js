dbc8b6346270f119e8ee54649752bcc7cfeac559
Trilok Chand <trilokc@Virtusa.com>
2018-09-11 17:27:46 +0530

    google client id

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
+
+//clientID : 664935293706-mbplk7vdaeipac6lkp80pn1k7493p2lu.apps.googleusercontent.com
+//clientSecret: Qx1s9vzNIj9qhg30lpScJSvt
+passport.use(new GoogleStrategy());
 
 app.get("/", (req, res) => {
     res.send({
