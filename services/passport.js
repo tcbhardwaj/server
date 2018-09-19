7467c2d1ab454fb94c88e66db8f9e97ccc4e0b65
Trilok Chand <trilokc@Virtusa.com>
2018-09-19 11:39:53 +0530

    getting value from array

--- services/passport.js
+++ services/passport.js
@@ -6,7 +6,7 @@ const keys = require('../config/keys');
 const User = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
-    done(null,user.id);
+    done(null,user[0].id);
 });
 passport.deserializeUser((id, done) => {
     User.findById(id).then(user => {
