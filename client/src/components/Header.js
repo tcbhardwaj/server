dbbda04a07fb494f801e330761fc5ba401997784
unknown <tcbhardwaj@gmail.com>
2018-10-08 22:43:06 +0530

    login and logout process complete

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,15 +2,29 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 
 class Header extends Component {
+
+    renderContent() {
+        switch(this.props.auth) {
+            case null:
+                return;
+            case false:
+                return <li><a href="/auth/google">Login With Google+</a></li>;
+            default:
+            return (
+                <div>
+                    <li>Welcome! {this.props.auth.displayName} </li>
+                    <li><a href="/api/logout">Logout</a></li>
+                </div>
+            );
+        }
+    }
     render() {
         return(
             <nav>
                 <div className="nav-wrapper">
                     <a className="left brand-logo"> Emaily </a>
                     <ul className="right">
-                        <li>
-                            <a>Login with Google+</a>
-                        </li>
+                        {this.renderContent()}
                     </ul>
                 </div>
             </nav>
@@ -18,7 +32,7 @@ class Header extends Component {
     }
 }
 
-function mapStateToProps(state) {
-    return {auth: state.auth};
+function mapStateToProps({auth}) {
+    return { auth };
 }
 export default connect(mapStateToProps)(Header);
