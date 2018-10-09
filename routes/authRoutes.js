bd85344d99f8ee13459e836b6bcc17de2c2b5381
Trilok Chand <trilokc@Virtusa.com>
2018-10-09 15:15:09 +0530

    express error resolve

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -26,7 +26,7 @@ module.exports = (app) => {
 
     if (process.env.NODE_ENV === 'production') {
         // Exprees will serve up production assets
-        app.use(express.static('client/build'));
+        app.use(app.static('client/build'));
       
         // Express serve up index.html file if it doesn't recognize route
         const path = require('path');
