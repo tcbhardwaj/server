4c369930c469211ab4569670d84c60ef0031b0ee
Trilok Chand <trilokc@Virtusa.com>
2018-10-09 15:51:12 +0530

    path.join()

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -32,11 +32,8 @@ module.exports = (app) => {
         // Express serve up index.html file if it doesn't recognize route
         const path = require('path');
         app.get('*', (req, res) => {
-          res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+          res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
         });
       }
-
-    // app.get('/*', (req, res) => {
-    //     res.send('welcome page');
-    // })
+    
 }
