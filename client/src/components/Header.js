269a7eb5168db06216bdb4c1a25dbfaee389fbf2
Trilok Chand <trilokc@Virtusa.com>
2018-10-09 11:58:52 +0530

    landing page changed

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 class Header extends Component {
@@ -22,7 +23,10 @@ class Header extends Component {
         return(
             <nav>
                 <div className="nav-wrapper">
-                    <a className="left brand-logo"> Emaily </a>
+                    <Link to={this.props.auth ? '/surveys' : '/'} 
+                        className="left brand-logo">
+                        Emaily 
+                    </Link>
                     <ul className="right">
                         {this.renderContent()}
                     </ul>
---
+++ client/src/components/Landing.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class Landing extends Component {
+
+    render() {
+        return (
+            <div style={{textAlign: 'center'}}>
+                <h1>Emaily</h1>
+                Collect feedback from user
+            </div>
+        );
+    }
+}
+
+export default Landing;
+
