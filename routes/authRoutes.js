31fc14a002b906888d009ed143b26d8514fa2e7d
unknown <tcbhardwaj@gmail.com>
2018-10-09 22:49:51 +0530

    prod

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -28,8 +28,8 @@ module.exports = (app) => {
 
     if (process.env.NODE_ENV === 'production') {
         // Exprees will serve up production assets
-        app.use(express.static('client/build'));
-        //app.use(express.static(path.join(__dirname, 'client/build')));
+        //app.use(express.static('client/build'));
+        app.use(express.static(path.join(__dirname, 'build')));
       
         // Express serve up index.html file if it doesn't recognize route        
         app.get('/*', (req, res) => {            
